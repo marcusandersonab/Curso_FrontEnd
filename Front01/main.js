362348148f0598d5dc9ad803bd9ef58a6851edb7
Marcus <marcus@dee.ufc.br>
2023-08-08 00:10:08 -0300

    identar

--- Front01/main.js
+++ Front01/main.js
@@ -6,24 +6,24 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAuto = `#som_${instrumento}`;
-    
-    tecla.onclick = function() {
+
+    tecla.onclick = function () {
         tocaSom(idAuto);
     }
     //console.log(contador);
 
-    tecla.onkeydown = function(evento) {
+    tecla.onkeydown = function (evento) {
         if ((evento.code === 'Space') || (evento.code === 'Enter')) {
             tecla.classList.add('ativa');
         }
     }
 
-    tecla.onkeyup = function() {
+    tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
     }
 }
 
-function tocaSom (seletorAudio) {
+function tocaSom(seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
     if (elemento && elemento.localName === 'audio') {
