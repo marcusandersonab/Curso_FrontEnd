108cbcc88119db65c7d9344be95056195415113b
Marcus <marcus@dee.ufc.br>
2023-11-22 23:06:28 -0300

    commit

--- Front01/main.js
+++ Front01/main.js
@@ -1,36 +1,33 @@
-
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAuto = `#som_${instrumento}`;
 
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAuto = `#som_${instrumento}`;
+  tecla.onclick = function () {
+    tocaSom(idAuto);
+  };
+  //console.log(contador);
 
-    tecla.onclick = function () {
-        tocaSom(idAuto);
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
     }
-    //console.log(contador);
+  };
 
-    tecla.onkeydown = function (evento) {
-        if ((evento.code === 'Space') || (evento.code === 'Enter')) {
-            tecla.classList.add('ativa');
-        }
-    }
-
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
 
 function tocaSom(seletorAudio) {
-    const elemento = document.querySelector(seletorAudio);
+  const elemento = document.querySelector(seletorAudio);
 
-    if (elemento && elemento.localName === 'audio') {
-        elemento.play();
-    }
-    else {
-        //alert ('Elemento não encontrado');
-        console.log('Elemento não encontrado ou seletor inválido');
-    }
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    //alert ('Elemento não encontrado');
+    console.log("Elemento não encontrado ou seletor inválido");
+  }
 }
